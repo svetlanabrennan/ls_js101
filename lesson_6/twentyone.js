6bf998a4ed03052bbbbc05af3aeaee30f10ea878
Svetlana Brennan <svetlana.svn@gmail.com>
2020-01-15 10:54:45 -0600

    Adding updated program

--- lesson_6/twentyone.js
+++ lesson_6/twentyone.js
@@ -1,17 +1,31 @@
 /*
-1. Initialize deck
-2. Deal cards to player and dealer
-3. Player turn: hit or stay
-   - repeat until bust or stay
+
 4. If player bust, dealer wins.
 5. Dealer turn: hit or stay
    - repeat until total >= 17
 6. If dealer busts, player wins.
 7. Compare cards and declare winner.
 */
+readline = require("readline-sync");
 
+const BUSTED_VALUE = 21;
 const SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"];
-const VALUES = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+const VALUES = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+const NUM_VALUES = {
+  2: 2, 
+  3: 3, 
+  4: 4, 
+  5: 5, 
+  6: 6,
+  7: 7,
+  8: 8, 
+  9: 9, 
+  10: 10, 
+  Jack: 10, 
+  Queen: 10, 
+  King: 10, 
+  Ace: 11 
+}
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
@@ -22,7 +36,7 @@ function InitializeDeck() {
   let deck = [];
   VALUES.forEach(value => {
     SUITS.forEach(suit => {
-      deck.push((`${value} of ${suit}`))
+      deck.push({suit: suit, value: value})
     });
   });
   return deck;
@@ -39,13 +53,67 @@ function updateDeck(card, deck) {
   deck.splice(index, 1);
 }
 
-function dealCards() {
+// deals cards to player or dealer
+function dealCards(hand) {
   selectedCard = selectRandomCard(fullDeck);
-  playerCards.push(selectedCard);
+  hand.push(selectedCard);
   updateDeck(selectedCard, fullDeck);
-  selectedCard = selectRandomCard(fullDeck);
-  playerCards.push(selectedCard);
-  updateDeck(selectedCard, fullDeck);
+}
+
+function displayCardName(card) {
+  return `${card.value} of ${card.suit}`
+}
+
+function displayInitialHands() {
+  prompt(`Dealer has: ${displayCardName(dealerCards[0])} & unknown card`);
+  prompt(`You have: ${displayCardName(playerCards[0])} and ${displayCardName(playerCards[1])}`);
+}
+
+// get hand total value
+function calculateHandValue(hand) {
+  let total = hand.reduce((sum, card) => {
+    return sum + NUM_VALUES[card.value];
+  }, 0);
+  // call function to adjust for ace
+  total = adjustForAce(hand, total);
+  return total;
+}
+
+// need to check if this function adjusts multiple aces 
+function adjustForAce(hand, total) {
+  hand.filter(card => {
+    if ((card.value === "Ace") && (total > 21)) {
+      total -= 10;
+    } 
+  });
+  return total;
+}
+
+function displayPlayerHandValue() {
+  prompt(`Your hand value is ${playerTotal}`);
+}
+
+function askPlayerHitStay() {
+  let hitStayAnswer
+  while (true) {
+    prompt("Would you like to hit or stay? 1 => hit or 2 => stay");
+    hitStayAnswer = readline.question().trim();
+    if (["1", "2"].includes(hitStayAnswer)) break;
+    prompt("Invalid answer. Press 1 => hit or 2 => stay.")
+    hitStayAnswer = readline.question().trim();
+  }
+  return hitStayAnswer;
+}
+
+function displayHand(hand) {
+  let result = hand.map(card => {
+    return displayCardName(card);
+  });
+  console.log(`Your have: ${result.join(", ")}`);
+}
+
+function busted(total) {
+  return total > BUSTED_VALUE
 }
 
 
@@ -56,20 +124,43 @@ let selectedCard;
 
 let playerCards = [];
 let dealerCards = [];
+let playerTotal = 0;
+let dealerTotal = 0;
+let hitStayAnswer;
 
-// deals cards to player or dealer
-function dealCards(hand) {
-  selectedCard = selectRandomCard(fullDeck);
-  hand.push(selectedCard);
-  updateDeck(selectedCard, fullDeck);
-  selectedCard = selectRandomCard(fullDeck);
-  hand.push(selectedCard);
-  updateDeck(selectedCard, fullDeck);
-}
+//console.log(fullDeck); 
 
 // deals two cards to player and dealer
 dealCards(playerCards);
+dealCards(playerCards);
+dealCards(dealerCards);
 dealCards(dealerCards);
 
-prompt(`Dealer has: ${dealerCards[0]} & unknown card}`);
-prompt(`You have: ${playerCards.join(" and ")}`);
+//console.log(playerCards)
+//console.log(dealerCards);
+
+// shows the first two cards dealt (hiding dealer's 2nd card)
+displayInitialHands();
+
+playerTotal = calculateHandValue(playerCards);
+console.log(`player total is: ${playerTotal}`);
+
+// show player total hand value
+displayPlayerHandValue();
+
+// player turn
+while (true) {
+  hitStayAnswer = askPlayerHitStay(); // ask if they want to hit or stay
+  if (hitStayAnswer === "2") break; // if stay
+  dealCards(playerCards); // hit function
+  displayHand(playerCards); // display hand
+  playerTotal = calculateHandValue(playerCards);
+  displayPlayerHandValue(); // show total
+  if (busted(playerTotal)) break;
+}
+
+if (busted(playerTotal)) {
+  prompt("You busted!");
+} else {
+  prompt("You chose to stay!");
+}
